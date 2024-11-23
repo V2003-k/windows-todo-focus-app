b1e903756941e0a4a7b9870fb78c7e53915add6f
Vishwajeet <v.khaple030203@gmail.com>
2024-11-24 01:44:43 +0530

    heatmap implementd

--- todo-focus-app/server/index.js
+++ todo-focus-app/server/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const http = require('http');
+const socketIo = require('socket.io');
 require('dotenv').config();
 
 const app = express();
+const server = require('http').createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
+});
 
 app.use(express.json());
 app.use(cors());
@@ -13,13 +22,25 @@ mongoose.connect(process.env.MONGODB_URI, {
     useUnifiedTopology: true,
 })
 .then(() => console.log('MongoDB Connected'))
-.catch(err => {
-  console.error('MongoDB connection error:', err);
-  process.exit(1);
-});
+.catch(err => console.log(err));
 
-app.get('/', (req, res) => {
-    res.send('Server is running');
+const todoRoutes = require('./routes/todoRoutes');
+app.use('/api/todos', todoRoutes);
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('newTodo', (todo) => {
+    socket.broadcast.emit('newTodo', todo);
+  });
+
+  socket.on('deleteTodo', (id) => {
+    socket.broadcast.emit('deleteTodo', id);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
 });
 
 const PORT = process.env.PORT || 5000;
--- todo-focus-app/server/routes/todoRoutes.js
+++ todo-focus-app/server/routes/todoRoutes.js
@@ -1,1 +1,45 @@
 const express = require('express');
+const router = express.Router();
+const Todo = require('../models/Todo');
+
+router.get('/', async (req, res) => {
+    try {
+      const todos = await Todo.find();
+      res.json(todos);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  });
+
+  router.post('/', async (req, res) => {
+    const { title } = req.body;
+    const newTodo = new Todo({ title });
+    try {
+      const savedTodo = await newTodo.save();
+      res.status(201).json(savedTodo);
+    } catch (err) {
+      res.status(400).json({ message: err.message });
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    try {
+      const { id } = req.params;
+      const updatedTodo = await Todo.findByIdAndUpdate(id, req.body, { new: true });
+      res.json(updatedTodo);
+    } catch (err) {
+      res.status(400).json({ message: err.message });
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+      const { id } = req.params;
+      await Todo.findByIdAndDelete(id);
+      res.json({ message: 'Todo deleted successfully' });
+    } catch (err) {
+      res.status(400).json({ message: err.message });
+    }
+});
+
+module.exports = router;
